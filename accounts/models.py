4cc3371cc7812c122b1590619666a34a035bfc9d
Apratim Das <apratimdas18@gmail.com>
2020-08-21 17:44:48 +0530

    Added initially

---
+++ accounts/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, null = True,blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, null = True)
+    phone = models.CharField(max_length=100, null = True)
+    email = models.CharField(max_length=100, null = True)
+    profile_pic = models.ImageField(default="profile1.png",null = True, blank = True)
+    created_at = models.DateTimeField(auto_now_add=True, null = True)
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length = 100, null = True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Outdoor', 'Outdoor')
+    )
+    name = models.CharField(max_length=100, null = True)
+    price = models.FloatField(null = True)
+    category = models.CharField(max_length=100, null = True, choices = CATEGORY)
+    description = models.CharField(max_length=100, null = True, blank = True)
+    created_at = models.DateTimeField(auto_now_add=True, null = True)
+    tag = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out For Delivery', 'Out For Delivery'),
+        ('Delivered', 'Delivered')
+    )
+    customer = models.ForeignKey(Customer, null=True, on_delete= models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete= models.SET_NULL)
+    created_at = models.DateTimeField(auto_now_add=True, null = True)
+    status = models.CharField(max_length=100, null = True, choices = STATUS)
+    
+    def __str__(self):
+        return self.product.name
